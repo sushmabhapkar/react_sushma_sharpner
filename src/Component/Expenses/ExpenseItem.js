22fad19cdf6487bcc2cf0700f8c62117bced7f1c
sushmabhapkar <sushmabhapkar244@gmail.com>
2024-03-05 22:11:00 +0530

    m2 task1(dom delete button)

--- src/Component/Expenses/ExpenseItem.js
+++ src/Component/Expenses/ExpenseItem.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React,{useState} from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+ const[isDelete,setIsDelete]=useState(false);
 
+ const deleteHandler=()=>
+ {
+
+    setIsDelete(true);
+ };
+ if(isDelete)
+ {
+    return null;
+ }
+  
 
     return (
-        <Card className="expense-item">
+        <div>
+        <Card className="expense-item" id={`expenses-${props.id}`}>
             <ExpenseDate date={props.date}/>
-             <div className="expense-item__description">
-               <h2>{props.title}</h2>
-               <div className="expense-item__price">${props.amount}</div>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+                <button onClick={deleteHandler}>Delete Expense</button>
             </div>
+            
         </Card>
+    </div>
     );
-
 }
 export default ExpenseItem;
