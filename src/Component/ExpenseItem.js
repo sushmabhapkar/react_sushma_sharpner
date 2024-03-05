778d2536545ee610fe9e7d83591d7d077358e0b2
sushmabhapkar <sushmabhapkar244@gmail.com>
2024-03-05 11:11:31 +0530

    m1 task7

---
+++ src/Component/ExpenseItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ExpenseItem=()=>
+{
+   return(
+   <div>
+    <h2>Expense Item</h2>
+     <ul>
+        <li>petrol Rs100</li>
+        <li>food Rs200</li>
+        <li>Movies Rs500</li>
+     </ul>
+   </div>
+   
+   );
+
+}
+export default ExpenseItem;
